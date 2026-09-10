e62b9b5f6ba75245513f2e34eeaac1771f347f1a
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-10 13:33:34 +0900

    form.js update

--- student_js/js/form.js
+++ student_js/js/form.js
@@ -69,12 +69,25 @@ function validateStudent(student) {
     //     alert("학번은 영문과 숫자만 입력 가능합니다.");
     //     return false;
     // }
-    
-    // 학번 형식 검사 (예: 영문과 숫자 조합)
-    if (student.studentNumber && !isValidStudentNumber(student.studentNumber)){
-        alert("올바른 학번 형식이 아닙니다.");
+
+    // if (!student.studentNumber) {
+    //     alert("학번을 입력해주세요.");
+    //     return false;
+    // } else if(!isValidStudentNumber(student.studentNumber)){
+    //      alert("올바른 학번 형식이 아닙니다.");
+    //     return false;
+    // }
+
+    if (!student.studentNumber || !isValidStudentNumber(student.studentNumber)) {
+        alert("학번을 입력하지 않으시거나 올바른 형식이 아닙니다.");
         return false;
     }
+
+    // 학번 형식 검사 (예: 영문과 숫자 조합)
+    // if (student.studentNumber && !isValidStudentNumber(student.studentNumber)){
+    //     alert("올바른 학번 형식이 아닙니다.");
+    //     return false;
+    // }
 
 
     if (!student.detailRequest.phoneNumber) {
