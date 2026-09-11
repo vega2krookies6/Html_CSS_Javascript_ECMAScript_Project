73e0884fc7ae1ee0babfd116cc988780ec6e1cde
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-11 13:24:39 +0900

    cancel btn activate

--- student_js/js/form.js
+++ student_js/js/form.js
@@ -121,6 +121,7 @@ async function editStudent(studentId) {
         editingStudentId = studentId;
         submitButton.textContent = '학생 수정';
         studentForm.scrollIntoView({ behavior: 'smooth' });
+        cancelButton.style.display = 'block';
     } catch (error) {
         console.error('Error:', error.message);
         showError(error.message);
