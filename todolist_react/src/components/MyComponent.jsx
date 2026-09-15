94dffb405b860ed29ac6cc8d621723c50643e0af
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 16:26:02 +0900

    MyCompFunc.jsx update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -9,6 +9,7 @@ class MyComponent extends Component {
     render() {
         const { name, age } = this.props; 
         const { value } = this.state;
+        
         return (
             <div>
                 <h2>클래스 형태의 컴포넌트</h2>
--- todolist_react/src/components/MyComponentFunc.jsx
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 
 const MyComponentFunc = ({ name, age, children }) => {
     const [value, setValue] = useState(0);
-    
+
     return (
         <div>
             <h2>함수 형태의 컴포넌트</h2>
             <h3>Hello! {name} / {age}</h3>
             {children}
+            <p>상태변수 value = {value}</p>
+            <button onClick={() => setValue(value + 1)}>증가</button>
         </div>
     );
 };
