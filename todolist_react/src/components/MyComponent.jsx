d1e670e209eb8b09061a17975b992c1589632395
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 15:54:53 +0900

    MyCompoent update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class MyComponent extends Component {
     render() {
+        const { name, age } = this.props; 
         return (
             <div>
                 <h2>클래스 형태의 컴포넌트</h2>
-                <h3>Hello! {this.props.name} / {this.props.age}</h3>
+                <h3>Hello! {name} / {age}</h3>
             </div>
         );
     }
