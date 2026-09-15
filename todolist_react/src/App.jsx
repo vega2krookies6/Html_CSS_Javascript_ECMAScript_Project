9eb0efcd5e93ed0d55479c0990b87e7e12858e19
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 15:57:41 +0900

    App.jsx update

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import MyComponent from './components/MyComponent'
+import MyComponentFunc from './components/MyComponentFunc'
 
 function App() {
 
   return (
     <>
-      <MyComponent name="홍길동" age={20} />
+      <MyComponent name="클래스형" age={20} />
+      <hr/>
+      <MyComponentFunc  name="함수형" age={30} />
     </>
   )
 }
