b3ba7250dd5f1fcb092e9fb788f59442bd18649d
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 15:47:27 +0900

    todolist_react add

---
+++ todolist_react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>클래스 형태의 컴포넌트</h2>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
