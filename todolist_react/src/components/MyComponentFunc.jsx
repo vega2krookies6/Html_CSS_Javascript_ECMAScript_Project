a8caa60c8a9e544ed9d3d4e07ad64d213eb6f67e
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 17:59:54 +0900

    spread operator add

--- todolist_react/src/components/MyComponentFunc.jsx
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -13,6 +13,7 @@ const MyComponentFunc = ({ name, age, children }) => {
     //event handler 함수
     const handleChange = (e) => {
         setInputs({
+            ...inputs,
             [e.target.name]: e.target.value
         });
     }
