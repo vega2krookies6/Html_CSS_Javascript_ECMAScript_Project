319dbec9a3919dbc3d75d2419e1e04b0435bab4d
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 16:06:38 +0900

    MyComp.jsx update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -8,11 +8,12 @@ class MyComponent extends Component {
 
     render() {
         const { name, age } = this.props; 
+        const { value } = this.state;
         return (
             <div>
                 <h2>클래스 형태의 컴포넌트</h2>
                 <h3>Hello! {name} / {age}</h3>
-                <p>상태변수 value = {this.state.value}</p>
+                <p>상태변수 value = {value}</p>
                 <button >증가</button>
             </div>
         );
