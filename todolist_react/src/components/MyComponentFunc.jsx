33e1cc988f8b43375431906c42ff6240735ffedd
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-15 16:57:41 +0900

    MyCompFunc.jsx update

--- todolist_react/src/components/MyComponentFunc.jsx
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -7,6 +7,13 @@ const MyComponentFunc = ({ name, age, children }) => {
     const [inputs, setInputs] = useState({
         message: '', username: ''
     });
+    //비구조화 할당
+    const { message, username } = inputs;
+
+    //event handler 함수
+    const handleChange = (e) => {
+
+    }
 
     return (
         <div>
@@ -15,6 +22,11 @@ const MyComponentFunc = ({ name, age, children }) => {
             {children}
             <p>상태변수 value = {value}</p>
             <button onClick={() => setValue(value + 1)}>증가</button>
+            <p>상태변수 message = {message}</p>
+            <input name="message" value={message} onChange={handleChange} />
+            <br />
+            <p>상태변수 username = {username}</p>
+            <input name="username" value={username} onChange={handleChange} />
         </div>
     );
 };
