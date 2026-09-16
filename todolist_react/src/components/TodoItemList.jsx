fa609830a734f6893cdadb1f2f562c8172b260e9
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 11:12:05 +0900

    TodoItemList => TodoItem.jsx call

--- todolist_react/src/components/TodoItemList.jsx
+++ todolist_react/src/components/TodoItemList.jsx
@@ -1,6 +1,12 @@
+import TodoItem from "./TodoItem";
+
 const TodoItemList = ({ myTodos, myToggle, myRemove }) => {
     return (
         <div>
+            <TodoItem text="안녕" />
+            <TodoItem text="리액트" />
+            <TodoItem text="반가워" />
+
         </div>
     );
 };
