c935269587d7d1aecb9163246326ac6f73669012
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 10:39:18 +0900

    App -> Form.jsx call

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Form from './components/Form'
 import TodoListTemplate from './components/TodoListTemplate'
 
 function App() {
 
   return (
     <>
-      <TodoListTemplate>
+      <TodoListTemplate form={<Form />}>
         템플릿 완성
       </TodoListTemplate>
 
