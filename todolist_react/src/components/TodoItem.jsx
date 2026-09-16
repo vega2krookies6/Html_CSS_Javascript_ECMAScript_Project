a3af2809528e6a6b49f93551947e5a3111caee6d
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 15:23:34 +0900

    TodoItem memo() add

--- todolist_react/src/components/TodoItem.jsx
+++ todolist_react/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
  
 const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
     return (
@@ -20,4 +21,9 @@ const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
     );
 };
  
-export default TodoItem;
+//export default TodoItem;
+// checked 가 바뀔 때만 다시 그린다
+export default memo(
+    TodoItem,
+    (prevProps, nextProps) => prevProps.checked === nextProps.checked
+);
