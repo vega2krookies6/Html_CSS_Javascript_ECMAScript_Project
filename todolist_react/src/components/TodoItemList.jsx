b112508ed87d19de6d3d363bfa81668b4e1a777d
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 14:05:33 +0900

    TodoItemList map() 반복

--- todolist_react/src/components/TodoItemList.jsx
+++ todolist_react/src/components/TodoItemList.jsx
@@ -1,6 +1,20 @@
 import TodoItem from "./TodoItem";
 
 const TodoItemList = ({ myTodos, myToggle, myRemove }) => {
+    // 배열의 값 하나하나를 화면 조각으로 바꾼다
+    const todoList = myTodos.map(
+        ({ id, text, checked }) => (
+            <TodoItem
+                id={id}
+                text={text}
+                checked={checked}
+                onToggle={myToggle}
+                onRemove={myRemove}
+                key={id}
+            />
+        )
+    );
+    
     return (
         <div>
             <TodoItem text="안녕" />
