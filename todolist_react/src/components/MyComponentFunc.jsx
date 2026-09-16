6f14c002c253fc875bbd5a87777a7d44b2727c4c
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 09:54:46 +0900

    setInputs call

--- todolist_react/src/components/MyComponentFunc.jsx
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -25,7 +25,11 @@ const MyComponentFunc = ({ name, age, children }) => {
     }
     const handleEnter = (e) => {
         if(e.keyCode === 13){
-
+            setValid(true);
+            setInputs({
+                ...inputs,
+                message: ''
+            });
         }
     }
     return (
