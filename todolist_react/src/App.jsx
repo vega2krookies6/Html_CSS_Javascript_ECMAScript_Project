fe13f300775347988048a158b745915aa6b37f35
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 14:49:08 +0900

    App 500

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -4,17 +4,22 @@ import Form from './components/Form'
 import TodoItemList from './components/TodoItemList'
 import TodoListTemplate from './components/TodoListTemplate'
 
+const initialTodos = new Array(500).fill(0).map(
+    (item, idx) => ({ id: idx, text: `일정 ${idx}`, checked: true })
+);
+
 function App() {
   // 입력칸에 지금 들어 있는 글자
   const [todo, setTodo] = useState("");
 
   // 할 일 목록. 처음에 세 개를 넣어 둔다.
-  const [todos, setTodos] = useState([
-    { id: 0, text: "리액트 소개", checked: false },
-    { id: 1, text: "리액트 구조", checked: true },
-    { id: 2, text: "리액트 사용", checked: false },
-  ]);
-
+  // const [todos, setTodos] = useState([
+  //   { id: 0, text: "리액트 소개", checked: false },
+  //   { id: 1, text: "리액트 구조", checked: true },
+  //   { id: 2, text: "리액트 사용", checked: false },
+  // ]);
+  const [todos, setTodos] = useState(initialTodos);
+  
   // 다음에 만들 할 일의 번호. 0,1,2 를 이미 썼으므로 3부터.
   const [nextId, setNextId] = useState(3);
 
