4c68e4825724843ab25ab883afd74fe971ac726b
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-16 09:46:12 +0900

    useRef hook

--- todolist_react/src/components/MyComponentFunc.jsx
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import './MyComponent.css';
 
 const MyComponentFunc = ({ name, age, children }) => {
     const [value, setValue] = useState(0);
@@ -7,6 +8,8 @@ const MyComponentFunc = ({ name, age, children }) => {
     const [inputs, setInputs] = useState({
         message: '', username: ''
     });
+    const [valid, setValid] = useState(false);
+    
     //비구조화 할당
     const { message, username } = inputs;
 
