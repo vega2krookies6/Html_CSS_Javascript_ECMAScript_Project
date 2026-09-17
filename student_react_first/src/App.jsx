174d79fd60d8db737e7bea9b363f2695406ed35e
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-17 11:24:33 +0900

    StudentTable call

--- student_react_first/src/App.jsx
+++ student_react_first/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { fetchStudents } from './api/studentApi';
 import './style.css'
+import StudentTable from './components/StudentTable';
 
 function App() {
   //상태 변수 선언
@@ -40,11 +41,27 @@ function App() {
     // 아래 주석은 ESLint 에게 "이 경고는 알고 있다"고 알려 주는 줄이다.
     // eslint-disable-next-line react-hooks/set-state-in-effect -- 처음 한 번 목록을 불러오는 것은 의도된 동작입니다
     loadStudents();
-  },[]);
+  }, []);
+
+  function handleEdit() {
+  }
+
+  function handleDelete() {
+  }
+
 
   return (
     <>
       <h1>학생 관리 시스템</h1>
+
+      <StudentTable
+        students={students}
+        loading={loading}
+        error={listError}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+
     </>
   )
 }
