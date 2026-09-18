92634b5b1de1a834c979964c1cdc7a66728a0026
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-18 15:59:36 +0900

    NotFoundPage update

--- student_react_router_axios/src/pages/NotFoundPage.jsx
+++ student_react_router_axios/src/pages/NotFoundPage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+ 
 function NotFoundPage() {
-    return <h2>없는 주소입니다</h2>;
+    return (
+        <div className="page">
+            <div className="not-found">
+                <h2>없는 주소입니다</h2>
+                <p>주소를 다시 확인해 주세요.</p>
+                <Link to="/">학생 목록으로 돌아가기</Link>
+            </div>
+        </div>
+    );
 }
  
 export default NotFoundPage;
