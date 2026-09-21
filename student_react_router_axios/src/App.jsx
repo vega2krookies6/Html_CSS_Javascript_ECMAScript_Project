6aab3562d097711f3701bc1c6dac5b0f4a5bb457
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-21 10:13:11 +0900

    App comp update

--- student_react_router_axios/src/App.jsx
+++ student_react_router_axios/src/App.jsx
@@ -21,6 +21,8 @@ import StudentListPage from "./pages/StudentListPage.jsx";
 import StudentFormPage from "./pages/StudentFormPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
+import AppMessage from "./components/AppMessage.jsx";
+
 // 지금 어느 모드로 도는지 (TEST / PROD)
 import { APP_MODE } from "./config.js";
 
@@ -65,6 +67,9 @@ function App() {
                     </NavLink>
                 </nav>
             </header>
+
+            {/* store 의 메시지를 그리는 자리. 어느 페이지에서든 여기 뜬다. */}            
+            <AppMessage />
 
             {/* Routes 안에서 주소와 맞는 Route 하나만 그려진다. */}
             <Routes>
